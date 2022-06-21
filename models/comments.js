637a623f17a484c342bdea9c461a06c54e4b0cf5
carolinexc <caroline@humanperspectives.com>
2022-06-21 18:43:29 -0400

    updated comments.js

--- models/comments.js
+++ models/comments.js
@@ -1,18 +1,11 @@
-const mongoose = require('mongoose')
-
-// In future I would keep 
+// Require mongoose
+const mongoose = require('mongoose') 
 
 const commentSchema = new mongoose.Schema({
     content: {type: String, required: true},
     rating: {type: Number, min: 1, max: 5, default: 5},
     // Reference the User schema, so it references the ID in the database
     userID: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
-    
-    // Comment schema is embedded in Attraction schema
-    // Moving forward, would use have key for comment ID in commentSchema, the RDS way
-    // Best option (1): When you save the comment, you apply the attraction ID to the comment
-    // In-between option (2): keep embedding in Attraction, update comment schema to have userID****
-    // Anytime you wanted to see all comments for a user, you could write a function for the query (where all match a certain user ID)
 })
 
 const Comment = mongoose.model('Comment', commentSchema)
