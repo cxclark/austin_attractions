980727047d48a6114de23b8e57d4b6d50274ac0e
carolinexc <caroline@humanperspectives.com>
2022-06-21 18:18:48 -0400

    updated seeds.js

--- db/seeds.js
+++ db/seeds.js
@@ -1,8 +1,15 @@
+// This file is used to seed the database
+// Run it by calling `node db/seeds.js`
+
+// Load environment variables in .env file to make them available
 const dotenv = require('dotenv')
 dotenv.config();
+// Require connection.js
 require('./connection')
+// Require the Attraction model
 const Attraction = require('../models/attractions');
 
+// Require the seed data in seeds.json
 const attractionSeeds = require('./seeds.json');
 
 // Delete all attractions in the database
