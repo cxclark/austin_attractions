586d57ccd1e74b6dc836da22f6026a0d9d61a6d8
carolinexc <caroline@humanperspectives.com>
2022-06-21 18:59:59 -0400

    updated commentRoutes.js

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,5 +1,8 @@
+// Require express
 const express = require('express')
+// Create a new router object
 const router = express.Router()
+// Require comments controller
 const commentCtrl = require('../controllers/commentController')
 
 router.post('/:id/comments', commentCtrl.create)
